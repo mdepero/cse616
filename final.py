e8b24040a3fd4030eea087f2aed5f892b4292b37
mdepero <deperomm@miamioh.edu>
2018-04-08 12:04:52 -0400

    Long teg term equilibrium with animation save working (requires some packages to be installed)

--- final.py
+++ final.py
@@ -4,38 +4,38 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
 #----------------------------set global variables-----------------------#
-rows = 40
-cols = 40
-time = 1000
+rows = 80
+cols = 80
+time = 50               # Amount of time to run the simulation for
 step = 1
 
-aniInterval = 300       # milliseconds between each animation frame
+aniInterval = 50        # milliseconds between each animation frame
 
 #=== Wolf ===#
-wolfProb = .03          # likelihood tile inits to wolf
-#= Wolf Birth Rate =#
+wolfProb = .05          # likelihood tile inits to wolf
 wolfMinInitAge = 4      # range of age for initial population
 wolfMaxInitAge = 6
+#= Wolf Birth Rate =#
 wolfMatureAge = 0       # age necessary to have a child
 wolfMinChildAge = 0     # range of age between which child leaves, initial is 0
 wolfMaxChildAge = 1
 wolfChildFood = 0       # the food ration necessary to have a child
 #= Wolf Death Rate =#
-wolfMaxFood = 5        # food gained when eating, dies at 0, initial is max
+wolfMaxFood = 5         # food gained when eating, dies at 0, initial is max
 wolfEatFood = 2         # food at which wolf needs to eat again (eat if foodRation <= wolfEatFood)
 
 #=== Sheep ===#
 sheepProb = .2          # likelihood tile inits to sheep
-#= Sheep Birth Rate =#
 sheepMinInitAge = 0     # range of age for initial population
 sheepMaxInitAge = 4
+#= Sheep Birth Rate =#
 sheepMatureAge = 1      # age necessary to have a child
 sheepMinChildAge = 0    # range of age between which child leaves, initial is 0
-sheepMaxChildAge = 2
+sheepMaxChildAge = 1
 sheepChildFood = 0      # the food ration necessary to have a child
 #= Sheep Death Rate =#
-sheepMaxFood = 4        # food gained when eating, dies at 0, initial is max
-sheepEatFood = 3        # food at which sheep needs to eat again
+sheepMaxFood = 5        # food gained when eating, dies at 0, initial is max
+sheepEatFood = 2        # food at which sheep needs to eat again
 grassMatureAge = 2      # age grass is edible, 0 after eaten, initial is rand between [0, max]
 
 
@@ -232,27 +232,32 @@ for i in range(0, time, step):
     # plt.matshow(outputAnimals[i])
     # plt.show()
 
-index = -1
 
-def generate_data():
-    global index
-    index += 1
-    return outputAnimals[index]
 
-def update(data):
-    mat.set_data(data)
-    return mat
 
-def data_gen():
-    while True:
-        yield generate_data()
+index = 0
+
+def update(i):
+    mat.set_array(outputAnimals[i])
+    return [mat]
+
+
 
 fig, ax = plt.subplots()
-mat = ax.matshow(generate_data())
+mat = ax.matshow(outputAnimals[0])
 plt.colorbar(mat)
-ani = animation.FuncAnimation(fig, update, data_gen, interval=aniInterval)
+
+# Use the below line to view the animation rather than save it
 plt.show()
 
+# Use the below lines to save the animation rather than view it
+# Writer = animation.writers['ffmpeg']
+# writer = Writer(fps=(int)(1000/aniInterval), metadata=dict(artist='Me'), bitrate=1800)
+# ani = animation.FuncAnimation(fig, update, frames=time, interval=aniInterval, blit=True) 
+# ani.save('try_animation.mp4', writer=writer)
+
+plt.close()
+plt.clf()
 plt.plot(range(0, time, step), wolfCount, color='red')
 plt.plot(range(0, time, step), sheepCount, color='green')
 # plt.plot(range(0, time, step), grassCount)
